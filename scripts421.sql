fb2e3c767c92c3c9bac4358c13214c7cd5d5853b
ivan <ivanov93@vk.com>
2022-11-02 13:06:05 +0300

    4.2. Управление схемами баз данных

--- scripts421.sql
+++ scripts421.sql
@@ -3,3 +3,6 @@ ALTER TABLE student
     ALTER column name set not null,
     add constraint unique_name unique (name),
     alter column age set default 20;
+
+ALTER TABLE faculty
+    add constraint unique_color unique (color);
